0ee3899c708de97b2fe2eaee713e31fb0574faa1
Chung Ik Yu <cjddlr1@hotmail.com>
2017-02-06 11:30:54 +0900

    Sidebar Added

---
+++ Sidebar/test.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.sidebar-button').click(function(){
+    $('.sidebar').css('left', '-=270px');
+  });
+});
+
+한번 누를 때
+- 오른쪽으로 나오기
+- button이 close로 바뀌기
+
+두번 누를 때
+- 왼쪽으로 들어가기
+- button이 open으로 바뀌기
